1fd61a239ca7f98f9d00a92329129e3aea93d384
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-24 18:40:56 +0530

    Added: Calculate Wages for a Month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,22 +1,26 @@
 const isFullTime = 1;
 const isPartTime = 2;
 const wagePerHour = 20;
+const maxWorkingDays = 20;
 
-let empHrs = 0, empWage;
+let empHrs = 0, empWage, monthlyWage = 0;
 
-let empCheck = Math.floor(Math.random() * 10) % 3;
+for(let day = 0; day < maxWorkingDays; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
 
-switch(empCheck) {
-    case isFullTime:
-        empHrs = 8;
-        break;
-    case isPartTime:
-        empHrs = 4;
-        break;
-    default :
-        empHrs = 0;
+    switch(empCheck) {
+        case isFullTime:
+            empHrs = 8;
+            break;
+        case isPartTime:
+            empHrs = 4;
+            break;
+        default :
+            empHrs = 0;
+    }
+
+    empWage = empHrs * wagePerHour;
+    monthlyWage += empWage;
 }
 
-empWage = empHrs * wagePerHour;
-
-console.log("Employee Wage:", empWage);
+console.log("Employee Monthly Wage:", monthlyWage);
