96425f2902750dbb7d41992c7e31312b57492108
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-24 18:26:51 +0530

    Added: Add Part time employee and wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,12 +1,17 @@
-const isPresent = 1;
+const isFullTime = 1;
+const isPartTime = 2;
 const wagePerHour = 20;
 
 let empHrs = 0, empWage;
 
-let empCheck = Math.floor(Math.random() * 10) % 2;
+let empCheck = Math.floor(Math.random() * 10) % 3;
 
-if(empCheck == isPresent) 
+if(empCheck == isFullTime) 
     empHrs = 8;
+else if(empCheck == isPartTime)
+    empHrs = 4;
+else
+    empHrs = 0;
 
 empWage = empHrs * wagePerHour;
 
